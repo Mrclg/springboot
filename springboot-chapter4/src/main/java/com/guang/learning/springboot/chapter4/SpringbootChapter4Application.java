213c94bde015580aa73330e9b79800a6b30e8668
chenlinguang <linguangchen@creditease.cn>
2019-03-21 16:41:36 +0800

    springboot日志管理

---
+++ springboot-chapter4/src/main/java/com/guang/learning/springboot/chapter4/SpringbootChapter4Application.java
@@ -0,0 +1,13 @@
+package com.guang.learning.springboot.chapter4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChapter4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootChapter4Application.class, args);
+	}
+
+}
---
+++ springboot-chapter4/src/main/java/com/guang/learning/springboot/chapter4/controller/DemoController.java
@@ -0,0 +1,9 @@
+package com.guang.learning.springboot.chapter4.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class DemoController {
+}
